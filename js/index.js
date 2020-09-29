4ed61b681898399e0a4ccddbcef9079bf44a3644
April Darger <april.m.darger@gmail.com>
2020-09-29 17:53:42 -0600

    completed 5 of 10 event listeners

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 
 // 1        mouseenter - a pointing device is moved onto the element that has the listener attached.
 const logoHead = document.querySelector('.logo-heading')
+
 logoHead.addEventListener('mouseenter', (event) => {
     logoHead.style.borderBottom = '5px dotted black'
 })
@@ -11,20 +12,36 @@ logoHead.addEventListener('mouseleave', (event) => {
     logoHead.style.borderBottom = 'none'
 })
 
-// 3        select - some text is being selected
-const h2Tags = document.querySelectorAll('body h2')
+// 3        click - a pointing device button has been pressed and released on an element
+const h2Tags = document.querySelectorAll('h2')
 
-document.querySelectorAll('body h2')
-    .forEach(elem => elem.addEventListener('select', event => {
-        event.style.color = 'pink'
+Array.from(h2Tags).forEach(elem => {
+    elem.addEventListener('click', event => {
+        elem.style.color = 'pink'
+    })
+})
+
+// 4        dblclick - a pointing device button is clicked twice on an element
+
+document.querySelectorAll('a')
+    .forEach(link => link.addEventListener('dblclick', event => {
+        link.style.color = 'pink'
     }))
 
+// 5        pointerleave - Pointing device is moved out of the hit-testing boundary.
+const body = document.querySelector('body')
 
+body.addEventListener('pointerleave', (event) => {
+    alert(`Don't go!`)
+})
 
+// 6        focus - An element has received focus (does not bubble).
+const introImg = document.querySelector('.intro img')
 
-// 4
-// 5
-// 6
+introImg.addEventListener('focus', (event) => {
+    event.target.style.opacity = '0.5'
+})
+
 // 7
 // 8
 // 9
